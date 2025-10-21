6c9c7837032964d7d1cf99ddac39a03723a2d3a2
zahirRC28 <zahirvalentino2812@gmail.com>
2025-10-21 19:49:00 +0200

    correacion y opciones para algunos ejercicios

--- practica1.js
+++ practica1.js
@@ -36,7 +36,9 @@ Escribe una función llamada mayorDeDos que reciba dos números y
 devuelva el mayor de los dos. Si son iguales, devuelve un mensaje indicando que son iguales.*/
 //no es recomendable hacer let num1 = 1;
 //no es recomendable hacer let num2 = 2;
+
 function mayorDeDos(num1 , num2){
+    //se podria parcear antes como num1=Number(num1);
     if(num1 == num2){
         return "los numeros son iguales";
     }else if(num1 > num2){
@@ -45,6 +47,22 @@ function mayorDeDos(num1 , num2){
         return num2 + " es mayor que " + num1;
     }
 };
+/*
+Otra forma de hacerlo
+Esto sera doble negacion que seria " si no no es un numero = !NaN "
+function mayorDeDos(num1 , num2){
+    if(!NaN(num1)&&!NaN(num2)){
+        if(num1 == num2){
+            return "los numeros son iguales";
+        }else if(num1 > num2){
+            return num1 + " es mayor que " + num2;
+        }else if(num2 > num1){
+            return num2 + " es mayor que " + num1;
+        }
+    }else
+        return "Los valores tienen que ser numericos"
+}; */
+
 console.log(mayorDeDos(1,2));
 console.log(mayorDeDos(5,3));
 
@@ -73,6 +91,8 @@ Nota >= 50 y < 70: "Regular"
 Nota < 50: "Insuficiente" */
 
 //no es recomendable hacer let nota = 10;
+/* La forma que hice inicalmente
+
 function clasificarNota(nota){
     if(nota < 50){
         return "Insuficiente";
@@ -83,7 +103,33 @@ function clasificarNota(nota){
     }else if(nota >= 90){
         return "Excelente"
     }
+}; 
+
+*/
+
+function clasificarNota(nota){
+    let mensaje;
+    if(nota >= 0 && nota <= 100){
+        if(nota >= 0 && nota < 50){
+        mensaje = "Insuficiente";
+        }else if(nota >= 50 && nota < 70){
+            mensaje = "Regular";
+        }else if(nota >= 70 && nota < 90){
+            mensaje = "Buena";
+        }else if(nota >= 90){
+            mensaje = "Excelente";
+        }
+    }else{
+        mensaje = "nota invalida";
+    }
+    return mensaje;
 };
+//No se debe usar con un switch por que solo es para igualdades estrictas
+
+let calificacion = clasificarNota(35);
+console.log(calificacion);
+
+
 console.log(clasificarNota(10));
 console.log(clasificarNota(52));
 console.log(clasificarNota(78));
